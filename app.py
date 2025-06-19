9f799de870ea47723c1c37954f5bff8b66cf3ed1
Krishnabhuvan <karanamkrishnabhuvan3@gmail.com>
2025-06-19 21:15:16 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, jsonify
+import joblib
+import numpy as np
+import pandas as pd
+from custom_transformers import CombinedAttributesAdder
+
+app = Flask(__name__)
+
+# Load model and pipeline
+model = joblib.load("models/house_price_model.pkl")
+pipeline = joblib.load("models/data_pipeline.pkl")
+
+FEATURE_COLUMNS = [
+    'longitude',
+    'latitude',
+    'housing_median_age',
+    'total_rooms',
+    'total_bedrooms',
+    'population',
+    'households',
+    'median_income',
+    'ocean_proximity'
+]
+
+@app.route('/')
+def home():
+    return render_template('index.html')   # <-- Homepage
+
+@app.route('/predict', methods=['GET'])
+def show_predict_form():
+    return render_template('predict.html')  # <-- Prediction Form Page
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.get_json()
+        df = pd.DataFrame([data], columns=FEATURE_COLUMNS)
+        transformed = pipeline.transform(df)
+        prediction = model.predict(transformed)[0]
+        return jsonify({'prediction': round(prediction, 2)})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
